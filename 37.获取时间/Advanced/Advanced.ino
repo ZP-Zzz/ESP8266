4c1880d5dcbb245efad484ab671d188041aed7cd
ZP-Zzz <85420973+ZP-Zzz@users.noreply.github.com>
2021-07-24 22:17:23 +0800

    Add files via upload

---
+++ 37.获取时间/Advanced/Advanced.ino
@@ -0,0 +1,39 @@
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
+
+const char *ssid     = "TP-LINK_46D02C";
+const char *password = "32416500";
+
+WiFiUDP ntpUDP;
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 60*60*8, 30*60*1000);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+
+  Serial.print("当前时间为：");
+  Serial.println(timeClient.getFormattedTime());
+  
+
+  delay(1000);
+}
